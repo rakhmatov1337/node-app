916b92c536fff21a5c65eedfed03537f0c129739
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2026-02-07 15:10:04 +0500

    First commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import { add, subtraction } from './utils/index.js';
+console.log(add(5, 3));
+console.log(subtraction(5, 3));
---
+++ src/utils/index.js
@@ -0,0 +1,2 @@
+export {add} from './function1.js'
+export {subtraction} from './function2.js'
